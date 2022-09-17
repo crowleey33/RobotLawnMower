a02ca8cf4de41139bd689dac4de994966e7c6991
crowleey33 <107879077+crowleey33@users.noreply.github.com>
2022-09-17 17:30:06 +0200

    Add files via upload

---
+++ Garden.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotLawnMower
+{
+    class Garden
+    {
+        public Garden()
+        {            
+            GardenSize = GenerateGardenSize();                 // generate garden dimensions
+            GardenBool = UploadGardenWithContent(GardenSize);  // generates grass and trees
+            Processed = GenerateProcessedMatrix(GardenSize);   // tracks how many times have the robot visited the actual coordinate
+        }
+
+        public bool[,] GardenSize { get; set; }
+        public bool[,] GardenBool { get; set; }
+        public int[,] Processed { get; set; }
+
+        public static bool[,] GenerateGardenSize()
+        {
+            bool[,] gardenSize = new bool[GenerateRandom().Next(10, 20), GenerateRandom().Next(10, 20)];
+            return gardenSize;
+        }
+
+        public static bool[,] UploadGardenWithContent(bool[,] gardenSize)
+        {
+            bool[,] gardenBool = new bool[gardenSize.GetLength(0), gardenSize.GetLength(1)];
+
+            for (int i = 0; i < gardenBool.GetLength(0); i++)
+            {
+                for (int j = 0; j < gardenBool.GetLength(1); j++)
+                {
+                    if (GenerateRandom().Next(0, 100) <= 80)
+                    {
+                        gardenBool[i, j] = true;
+                    }
+                    else
+                    {
+                        gardenBool[i, j] = false;
+                    }
+                }
+            }
+            gardenBool[1, 1] = true;
+            return gardenBool;
+        }
+
+        public static int[,] GenerateProcessedMatrix(bool[,] gardenSize)
+        {
+            int[,] processed = new int[gardenSize.GetLength(0), gardenSize.GetLength(1)];
+            
+            for (int i = 0; i < gardenSize.GetLength(0); i++)
+            {
+                for (int j = 0; j < gardenSize.GetLength(1); j++)
+                {
+                    processed[i, j] = 0; 
+                }
+            }
+            return processed;
+        }
+
+        public static Random GenerateRandom()
+        {
+            return new Random();
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace RobotLawnMower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Garden garden = new Garden();
+            Robot robot = new Robot(1,1);
+
+            int test = garden.GardenSize.GetLength(0);
+            int test1 = garden.GardenSize.GetLength(1);
+            Console.WriteLine(test + " " + test1);
+
+            for (int i = 0; i < test; i++)
+            {
+                for (int j = 0; j < test1; j++)
+                {
+                    if (garden.GardenBool[i, j] == true)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkGreen;
+                        Console.Write("0 ", "\t");                        
+                    }                    
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.Write("1 ");                        
+                    }                    
+                }
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine();
+            }
+
+
+
+            while (TheGardenIsReady(garden))
+            {
+                if (garden.Processed[robot.PositionX,robot.PositionY+1] == 0 && garden.GardenBool[robot.PositionX, robot.PositionY + 1] != false)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    robot.PositionY++;
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+
+                }
+                else if (garden.Processed[robot.PositionX-1, robot.PositionY + 1] == 0 && garden.GardenBool[robot.PositionX-1, robot.PositionY + 1] != false)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    robot.PositionY++;
+                    robot.PositionX--;
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                }
+
+                else if (garden.Processed[robot.PositionX + 1, robot.PositionY + 1] == 0 && garden.GardenBool[robot.PositionX + 1, robot.PositionY + 1] != false)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    robot.PositionY++;
+                    robot.PositionX++;
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                }
+
+
+
+
+            }
+
+            bool TheGardenIsReady(Garden garden)
+            {
+                bool isReady = false;
+                for (int i = 0; i < garden.GardenSize.GetLength(0); i++)
+                {
+                    for (int j = 0; j < garden.GardenSize.GetLength(1); j++)
+                    {
+
+                        if (garden.Processed[i, j] == 0)
+                        {
+                            isReady= false;
+                        }
+                        else
+                        {
+                            isReady= true;
+                        }
+                    }
+                }
+                return isReady;
+            }
+
+           void RevealSurrounding(Robot robot, Garden garden)
+            {
+
+                if (garden.GardenBool[robot.PositionX-1,robot.PositionY-1] == true)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                }
+
+                if (garden.GardenBool[robot.PositionX - 1, robot.PositionY ] == true)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                }
+
+                if (garden.GardenBool[robot.PositionX - 1, robot.PositionY +1] == true)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                }
+
+               
+
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
